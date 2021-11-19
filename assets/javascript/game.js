c29b9f2f6b95bda7da0812ffd8004dcc92cf221f
rowgregory <rowgregory@gmail.com>
2021-11-18 21:59:51 -0500

    Make nav sticky

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
+$(document).ready(function () {
+  $('.scrollspy').scrollSpy();
+});
 
-  
-  
-  AOS.init();
+$(window).scroll(() => {
+  const navbar = document.getElementById('navbarsticky');
+  const scroll = $(window).scrollTop();
 
+  if (scroll > window.innerHeight) {
+    navbar.classList.add('makesticky');
+    navbar.classList.remove('unstick');
+  } else {
+    navbar.classList.add('unstick');
+    navbar.classList.remove('makesticky');
+  }
+});
 
-
-
+AOS.init();
